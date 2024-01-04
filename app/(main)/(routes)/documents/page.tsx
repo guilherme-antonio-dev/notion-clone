75ed78a6023431d34de2c55bbe13aeed6e7caabe
guilherme-antonio-dev <guilherme.antonio.dev@gmail.com>
2024-01-03 22:00:02 -0300

    user settings

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,21 @@
+"use client"
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const Documents = () => {
+    const { user } = useUser()
     return ( 
-        <div>
-            Documents
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image src="/empty.png" height="300" width="300" alt="Empty" className="dark:hidden"/>
+            <Image src="/empty-dark.png" height="300" width="300" alt="Empty" className="hidden dark:block"/>
+            <h2 className="text-lg font-medium">Bem vindo ao Notion do {user?.firstName}</h2>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2" />
+                Criar um documento
+            </Button>
         </div>
      );
 }
